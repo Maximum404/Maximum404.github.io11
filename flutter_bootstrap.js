7626573625b05a603e8bd6be7a762e09b3485cd6
Max404 <118112393+Maximum404@users.noreply.github.com>
2024-07-23 22:07:02 +0400

    Add files via upload

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f6344b75dcf861d8bf1f1322780b8811f982e3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3938593235"
+    serviceWorkerVersion: "2320420675"
   }
 });
